dde5d0eaf390e1f55f46b51528682c2e96ca3ae4
Vicky1347 <110741340+Vicky1347@users.noreply.github.com>
2023-06-05 23:17:06 +0530

    Update index.js

--- index.js
+++ index.js
@@ -6,52 +6,69 @@ for(var i=0;i<7;i++)
     function handleclick()
     {
         var buttoninnerHTML= this.innerHTML;
+        
+        this.style.color="red";
+        makesound(buttoninnerHTML);
 
-        switch(buttoninnerHTML)
-        {
-            case "w":
-                var tom1= new Audio('tom-1.mp3');
-                tom1.play()
-                break;
-
-            case "a":
-                var tom2= new Audio('tom-2.mp3');
-                tom2.play()
-                break;
-
-            case "s":
-                var tom3= new Audio('tom-3.mp3');
-                tom3.play()
-                break;
-
-            case "d":
-                var tom4= new Audio('tom-4.mp3');
-                tom4.play()
-                break;   
-                
-            case "j":
-                var snare= new Audio('snare.mp3');
-                snare.play()
-                break;
-            case "k":
-                var kick= new Audio('kick-bass.mp3');
-                kick.play()
-                break;
-
-            case "l":
-            var crash= new Audio('crash.mp3');
-            crash.play()
-            break;
-
-            default:
-                alert("wrong click");
-        }
     }
 
 
 
 }
+document.addEventListener("keydown",keyboard);
 
+    function keyboard(event)
+    {
+        makesound(event.key);
+    }
+
+
+function makesound(key)
+{
+
+    switch(key)
+    {
+        case "w":
+            var tom1= new Audio('sounds/tom-1.mp3');
+            tom1.play()
+            break;
+
+        case "a":
+            var tom2= new Audio('sounds/tom-2.mp3');
+            tom2.play()
+            break;
+
+        case "s":
+            var tom3= new Audio('sounds/tom-3.mp3');
+            tom3.play()
+            break;
+
+        case "d":
+            var tom4= new Audio('sounds/tom-4.mp3');
+            tom4.play()
+            break;   
+            
+        case "j":
+            var snare= new Audio('sounds/snare.mp3');
+            snare.play()
+            break;
+
+        case "k":
+            var kick= new Audio('sounds/kick-bass.mp3');
+            kick.play()
+            key.style.color="grey";
+
+            break;
+
+        case "l":
+        var crash= new Audio('sounds/crash.mp3');
+        crash.play()
+        break;
+
+        default:
+            alert("wrong click");
+    }
+}
 
 // alert("hello p");
 
